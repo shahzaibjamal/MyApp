58b07f1a7a0e574a5889a68430e1552f3bc6183c
shahzaib <shahzaib@shahzaib-Aspire-5715Z>
2013-04-29 18:53:25 +0500

    deleted reduntant

--- MyApp/src/com/example/myapp/Validate.java
+++ MyApp/src/com/example/myapp/Validate.java
@@ -1,15 +1,24 @@
 package com.example.myapp;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 import com.google.android.maps.GeoPoint;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -25,7 +34,7 @@ public class Validate extends Activity {
 	PlacesList nearPlaces;
 	double[] myCood;
 	double[] destinationCood;
-	
+	String result;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,23 @@ public class Validate extends Activity {
 		String destination = intent.getStringExtra("Destination");
 		myCood = intent.getExtras().getDoubleArray("myCood");
 		textView = (TextView)findViewById(R.id.textView3);
+		
+//		String returned = intent.getStringExtra("result");
+	
+	//		Log.d("returned result", returned);
+		SearchLocation s = new SearchLocation(destination,this);
+		s.execute();
+		try {
+			String sss = s.get();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ExecutionException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+			
+		Log.d("values returned", result);
 		
 		Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
 		
@@ -56,25 +82,7 @@ public class Validate extends Activity {
 				e.printStackTrace();
 			}
 		}
-
-/*	
-		googlePlaces = new GooglePlaces();
-		
-		try {
-			String types = "bus_station"; // Listing places only cafes, restaurants
-			
-			// Radius in meters - increase this value if you don't find any places
-			double radius = 1000; // 1000 meters
-			
-			// get nearest places
-			nearPlaces = googlePlaces.search(currLat,currLat, radius, types);
-			
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-*/		
+	
 		destinationCood = new double[2];
 		destinationCood[0] = currLat;
 		destinationCood[1] = currLong;		
@@ -94,5 +102,141 @@ public class Validate extends Activity {
 		getMenuInflater().inflate(R.menu.validate, menu);
 		return true;
 	}
+	
+	 private class SearchLocation extends AsyncTask<Void, Void, String>{
+		    private ProgressDialog progressDialog;
+
+		    DataTest d1;
+		    String destination;
+		    Cursor cursor;
+		    Context context;
+		    
+		    SearchLocation(String destination, Context context){
+		    	this.destination = destination;
+		    	result = "";
+		    	this.context = context;
+		    }
+		    
+		    @Override
+		    protected void onPreExecute() {
+		        // TODO Auto-generated method stub
+		        super.onPreExecute();
+		        progressDialog = new ProgressDialog(Validate.this);
+		        progressDialog.setMessage("Fetching Location, Please wait...");
+		        progressDialog.setIndeterminate(true);
+		        progressDialog.show();
+		    }
+		    
+		    @Override
+		    protected String doInBackground(Void... params) {
+		    	d1 = new DataTest(Validate.this);
+		    	
+		    	int destinationLength;
+		    	int dataLength;
+		    	try {
+					d1.createDataBase();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+		    	int distance = -1;
+		    	double max = -5;
+		    	int index = -1;
+		    	double div = -.01;
+		    	SQLiteDatabase db = d1.openDataBase();
+		    	//SQLiteDatabase db = d1.getInstance();
+		    	
+		    	
+		    	
+		    	System.out.println(DatabaseUtils.queryNumEntries(db,"route"));
+		    	
+		    	for(int i = 1;i<18;i++){
+		    		cursor = db.query("route", new String[] { "_id","bus_number","source","destination"}, "_id=?",
+		    				new String[]{String.valueOf(i)}, null, null, null, null);
+		    		if (cursor != null)
+		    			cursor.moveToFirst();
+		    		
+		    		String a = cursor.getString(2);
+		    		String temp1 = destination.toLowerCase();
+		    		String temp2 = a.toLowerCase();
+		    		distance = computeLevenshteinDistance(temp1,temp2);
+		    		
+		    		destinationLength = destination.length();
+		    		dataLength = a.length();
+		    		
+		    		if(distance != 0){
+		    			div = (((dataLength-distance)/distance)*dataLength);
+		    		}
+		    		
+		    		System.out.println(a + " location "+ distance+ " distance " + " div " +div);
+		    		
+		    		if(destinationLength < dataLength  && max < div){
+		    			max = div;
+		    			index = i;
+		    			System.out.println(div + " location " + a);
+		    		}
+		    		if(distance == 0){
+		    			index = i;
+		    		}
+		    	}
+		    	
+		    	if(index != -1){
+		    		cursor = db.query("route", new String[] { "_id","bus_number","source","destination"}, "_id=?",
+		    				new String[]{String.valueOf(index)}, null, null, null, null);
+		    		if (cursor != null)
+		    			cursor.moveToFirst();
+		    		
+		    		result = cursor.getString(2);
+		    	}
+		    	db.close();
+		    	if(!result.equals("")){
+		    		Log.d("return ","return value contained " + result);
+		    		return result;
+		    		
+		    	}else{
+		    		Log.d("not return","null");
+		    		
+		    		return null;
+		    	}
+		    }
+		    @Override
+		    protected void onPostExecute(String result1) {
+		        progressDialog.dismiss();
+		        result = result1;
+		        super.onPostExecute(result1);   
+
+		    }
+		}
+	 
+	 private int minimum(int a, int b, int c) {
+         return Math.min(Math.min(a, b), c);
+	 }
+
+	 public int computeLevenshteinDistance(CharSequence str1, CharSequence str2) {
+         int[][] distance = new int[str1.length() + 1][str2.length() + 1];
+
+         for (int i = 0; i <= str1.length(); i++)
+                 distance[i][0] = i;
+         for (int j = 1; j <= str2.length(); j++)
+                 distance[0][j] = j;
+
+         for (int i = 1; i <= str1.length(); i++)
+                 for (int j = 1; j <= str2.length(); j++)
+                         distance[i][j] = minimum(
+                                         distance[i - 1][j] + 1,
+                                         distance[i][j - 1] + 1,
+                                         distance[i - 1][j - 1]
+                                                         + ((str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0
+                                                                         : 1));
+
+         return distance[str1.length()][str2.length()];
+ }
+
+	 
+	 
+	
+	
 
 }
