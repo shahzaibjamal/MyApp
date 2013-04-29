2395c03e58451e300b21151b9192f51c603db4c0
shahzaib <shahzaib@shahzaib-Aspire-5715Z>
2013-04-29 18:50:52 +0500

    added database initialization/validity

--- MyApp/src/com/example/myapp/MainActivity.java
+++ MyApp/src/com/example/myapp/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.myapp;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
-
-import com.google.android.maps.GeoPoint;
+import java.util.concurrent.ExecutionException;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,14 +27,26 @@ public class MainActivity extends Activity {
 	double currLat;
 	TextView textView;
 	public static String add;
+	String result;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		connection = new GPSValidate(this);
-
-
+		
+		Thread t1 = new Thread(new db());
+		t1.start();
+		try {
+			t1.join();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		
 		if(connection.canGetLocation()){
 			currLong = connection.getLongitude();
 			currLat = connection.getLatitude();
@@ -48,9 +65,11 @@ public class MainActivity extends Activity {
 	                		
 	                	}
 	            }
+	            EditText t = (EditText) findViewById(R.id.editText1);
+	            t.setText("Regal");
 	            textView = (TextView) findViewById(R.id.TextView2);
 	            textView.setText(add);
-	            textView.setTextSize(25);
+	            textView.setTextSize(20);
 	       //     Toast.makeText(getBaseContext(), add, Toast.LENGTH_SHORT).show();
 	         } catch (IOException e) {                
 	                e.printStackTrace();
@@ -60,8 +79,8 @@ public class MainActivity extends Activity {
 
 	public void onClick(View view){
 		
-		//GeoPoint myGeoPoint = new GeoPoint((int)currLat,(int)currLong);
 		Intent intent = new Intent(this,Validate.class);
+		
 		EditText editText = (EditText) findViewById(R.id.editText1);
 		String destination = editText.getText().toString();
 		intent.putExtra("Destination", destination);
@@ -70,4 +89,25 @@ public class MainActivity extends Activity {
 		startActivity(intent);
 	}
 
+	
+	public class db implements Runnable{
+
+		DataTest d1;
+		SQLiteDatabase db;
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			d1 = new DataTest(MainActivity.this);
+			
+			
+			try {
+				d1.createDataBase();
+				System.out.println("Database created");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
